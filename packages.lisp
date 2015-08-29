84fff56360148d4c78313cde35113aeee62eddfb
the Phoeron <thephoeron@users.noreply.github.com>
2015-08-29 12:39:27 -0400

    Update packages.lisp

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,13 @@
 
 (in-package :cl-user)
 
+(defpackage cl-datomic
+  (:nicknames :datomic)
+  (:use :cl :cl-user :cl-datalog :cl-edn)
+  (:export #:*cl-datomic-version*))
+
+(in-package :cl-datomic)
+
+(defparameter *cl-datomic-version* #.cl-datomic-asd:*cl-datomic-version*)
+
 ;; EOF
