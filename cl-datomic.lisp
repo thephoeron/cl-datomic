c2eefe879de2b56ab278fbde803ff3c0627a2b11
the Phoeron <thephoeron@users.noreply.github.com>
2015-08-30 12:15:16 -0400

    Update cl-datomic.lisp

--- cl-datomic.lisp
+++ cl-datomic.lisp
@@ -2,4 +2,10 @@
 
 (in-package :cl-datomic)
 
+(defun global-connect (&key (host "localhost") (port 8080))
+  "Globally connect application to Datomic.")
+
+(defun global-disconnect ()
+  "If possible, globally disconnect from Datomic server. Else do nothing.")
+
 ;; EOF
